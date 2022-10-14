d771a833efa718ba1db05a5bf4c87b46bf9e44c0
pasha <tommyv2124@gmail.com>
2022-10-14 22:27:25 +0300

    "pervoe zadanie"

--- Test1.java
+++ Test1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Test1 {
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Input number:");
@@ -10,8 +10,14 @@ public class Test1 {
         int a = sc.nextInt();
         int number = sc.nextInt();
 
-        double x = a % Math.abs(number);
+        System.out.println(deleniePoModulyu(a, number));
+    }
 
-        System.out.println(x);
+    public static double deleniePoModulyu(int value, int chislo) {
+        double x = 0;
+        if (value >= 0) {
+            x = value % Math.abs(chislo);
+        }
+        return x;
     }
 }
